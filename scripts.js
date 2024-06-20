a17fdd1efffc637029ee14f427fa25e83eb6dac7
esdras fernandes <fernandesesdras20@gmail.com>
2024-06-20 20:19:27 -0300

    Atualizando Portfolio

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.link-menu');
+
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
+});
